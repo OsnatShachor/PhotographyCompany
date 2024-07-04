4941390a67fa8a11cb66843c6257fb69d67b02f1
OsnatShachor <osnaty16@gmail.com>
2024-07-04 23:53:08 +0300

    router and controller of photographer management

--- server/controllers/PhotographerManagementController.js
+++ server/controllers/PhotographerManagementController.js
@@ -1,24 +1,24 @@
 const PhotographerManagementModel = require('../models/PhotographerManagementModel');
 
 async function updateAbout(req, res) {
-  const photographerID = req.body.id;
-  const aboutMe = req.body.aboutMe;
-  try {
-    await PhotographerManagementModel.updateAbout(photographerID, aboutMe);
-    res.status(200).json({ message: 'About me updated successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to update about me' });
-  }
-};
-
+    const photographerID = req.body.id;
+    const aboutMe = req.body.aboutMe;
+    try {
+        await PhotographerManagementModel.updateAbout(photographerID, aboutMe);
+        res.status(200).json({ message: 'About me updated successfully' });
+    } catch (error) {
+        throw error;
+    }
+}
 
 async function checkIfPhotographerActive(req, res) {
-  const photographerID = req.params.id; 
-  try {
-    const isActive = await PhotographerManagementModel.checkIfPhotographerActive(photographerID);
-    res.status(200).json(isActive); 
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to check' });
-  }
+    const photographerID = req.params.id;
+    try {
+        const isActive = await PhotographerManagementModel.checkIfPhotographerActive(photographerID);
+        res.status(200).json(isActive);
+    } catch (error) {
+        throw error;
+    }
 }
-module.exports = {updateAbout, checkIfPhotographerActive }
+
+module.exports = { updateAbout, checkIfPhotographerActive };
--- server/routs/PhotographerManagementRouter.js
+++ server/routs/PhotographerManagementRouter.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const PhotographerManagementController = require('../controllers/PhotographerManagementController');
+const controller = require('../controllers/PhotographerManagementController');
 
-router.post('/:id/update-about', PhotographerManagementController.updateAbout); 
+router.post('/:id/update-about', async (req, res) => {
+    try {
+        await controller.updateAbout(req, res);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to update about me' });
+    }
+});
 
-router.get('/:id', PhotographerManagementController.checkIfPhotographerActive);
+router.get('/:id', async (req, res) => {
+    try {
+        await controller.checkIfPhotographerActive(req, res);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to check' });
+    }
+});
 
 module.exports = router;
